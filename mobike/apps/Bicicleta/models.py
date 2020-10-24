33d1a3480b258606cd02b995804c97b9df3dd04a
eribelen <eri.avalos@alumnos.duoc.cl>
2020-10-24 19:17:25 -0300

    Agregar, editar, listar y eliminar bicicletas

--- mobike/apps/Bicicleta/models.py
+++ mobike/apps/Bicicleta/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-MENTENCION_CHOICE = [(1,'Si'),(2,'No')]
+MENTENCION_CHOICE = [('si','Si'),('no','No')]
 
 class Bicicleta(models.Model):
     marca = models.CharField(max_length=15)
     modelo = models.CharField(max_length=20)
-    mantencion = models.CharField(max_length=2,choices=MENTENCION_CHOICE, default=2) 
+    mantencion = models.CharField(max_length=2,choices=MENTENCION_CHOICE, default='no') 
     
     def __str__(self):
         return self.marca
--- mobike/apps/Bicicleta/views.py
+++ mobike/apps/Bicicleta/views.py
@@ -13,7 +13,7 @@ def agregar_bicicleta(request):
         if form.is_valid():
             model_instance = form.save(commit=False)
             model_instance.save()
-            return redirect("/agregar_bicicleta")
+            return redirect("/listar_bicicletas")
     else:
         form = BicicletaForm()
         return render(request, "Bicicleta/agregar_bicicleta.html", {'form': form})
@@ -44,6 +44,7 @@ def editar_bicicleta(request, bicicleta_id):
             instancia = form.save(commit=False)
             # Podemos guardar cuando queramos
             instancia.save()
+            return redirect('listar_bicicletas')
 
     # Si llegamos al final renderizamos el formulario
     return render(request, "Bicicleta/editar_bicicleta.html", {'form': form})
