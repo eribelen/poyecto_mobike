f1676b113396af575864a40ae3d38c50d3842831
eribelen <eri.avalos@alumnos.duoc.cl>
2020-10-30 11:22:02 -0300

    Modificación CRUD estacionamientos

--- mobike/apps/Bicicleta/forms.py
+++ mobike/apps/Bicicleta/forms.py
@@ -6,14 +6,16 @@ MENTENCION_CHOICE = [(1,'Si'),(2,'No')]
 class BicicletaForm(forms.ModelForm):
     class Meta:
         model = Bicicleta
-        fields = ['marca', 'modelo', 'mantencion']
+        fields = ['marca', 'modelo', 'mantencion', 'estacionamiento']
         labels = {
             'marca': 'Marca',
             'modelo': 'Modelo',
             'mantencion': 'Mantención',
+            'estacionamiento': 'Estacionamiento Asignado',
         }
         widgets = {
             'marca': forms.TextInput(attrs={'class': 'mb-2 form-control'}),
             'modelo': forms.TextInput(attrs={'class': 'mb-2 form-control'}),
             'mantencion': forms.Select(choices=MENTENCION_CHOICE, attrs={'class': 'mb-2 form-control'}),
+            'estacionamiento': forms.TextInput(attrs={'class': 'mb-2 form-control'}),
         }
